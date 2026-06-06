8e578fda7dcba3c7893a08573b9afa4df8baa267
faustjoe <joeyfausto107@gmail.com>
2026-06-06 20:59:54 +0000

    AB#4336 Add Contact Us Form (#4)
    Database
    <img width="1075" alt="image.png" src="attachments/6dde631a-5839-4f54-94e6-59ea285b04ea">
    
    Reviewed-on: https://git.websurfingstudios.com/Phase1/PetBoarding_JoeyFausto/pulls/4
    Reviewed-by: Daniel Pagano <djpagano@loyola.edu>
    Co-authored-by: faustjoe <joeyfausto107@gmail.com>
    Co-committed-by: faustjoe <joeyfausto107@gmail.com>

--- WebAppTemplate/Controllers/ContactUsController.cs
+++ WebAppTemplate/Controllers/ContactUsController.cs
@@ -1,10 +1,47 @@
 using System.Web.Mvc;
+using WebAppTemplate.Models;
+using WebAppTemplate.ViewModels;
 
 namespace WebAppTemplate.Controllers
 {
     public class ContactUsController : Controller
     {
+        // GET: /ContactUs
         public ActionResult Index()
+        {
+            var vm = new ContactUsSubmissionVM();
+            return View(vm);
+        }
+
+        // POST: /ContactUs
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(ContactUsSubmissionVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var submission = new ContactUsModel
+            {
+                FirstName = vm.FirstName,
+                LastName = vm.LastName,
+                Email = vm.Email,
+                Phone = vm.Phone,
+                Subject = vm.Subject,
+                Message = vm.Message
+            };
+
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+            dbContext.ContactUsModels.Add(submission);
+            dbContext.SaveChanges();
+
+            return RedirectToAction("ThankYou");
+        }
+
+        // GET: /ContactUs/ThankYou
+        public ActionResult ThankYou()
         {
             return View();
         }
---
+++ WebAppTemplate/ViewModels/ContactUsSubmissionVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTemplate.ViewModels
+{
+    public class ContactUsSubmissionVM
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Subject { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string Message { get; set; }
+    }
+}
