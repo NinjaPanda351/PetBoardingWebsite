361dfd6edb42c0e3e2f9a809a90178001e1f285b
faustjoe <joeyfausto107@gmail.com>
2026-06-19 16:26:21 +0000

    AB#4342 - Implement Email in App (#8)
    <img width="452" alt="image.png" src="attachments/18cb4c63-b4b1-4f3b-b74d-fef04dab6d85">
    
    <img width="418" alt="image.png" src="attachments/77259d5e-dc80-4ff2-920f-a459c6937f3e">
    
    Reviewed-on: https://git.websurfingstudios.com/Phase1/PetBoarding_JoeyFausto/pulls/8
    Reviewed-by: Daniel Pagano <djpagano@loyola.edu>
    Co-authored-by: faustjoe <joeyfausto107@gmail.com>
    Co-committed-by: faustjoe <joeyfausto107@gmail.com>

--- WebAppTemplate/App_Start/IdentityConfig.cs
+++ WebAppTemplate/App_Start/IdentityConfig.cs
@@ -1,25 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
 using WebAppTemplate.Models;
 
 namespace WebAppTemplate
 {
-    public class EmailService : IIdentityMessageService
+    
+public class EmailService : IIdentityMessageService
     {
         public Task SendAsync(IdentityMessage message)
         {
-            // Plug in your email service here to send an email.
-            return Task.FromResult(0);
+            return SendEmailAsync(message.Destination, message.Subject, message.Body);
+        }
+
+        public Task SendEmailAsync(string destination, string subject, string body)
+        {
+            MailMessage mailMessage = EmailHelpers.GenerateMailMessage(destination, subject, body);
+            return EmailHelpers.GetSmtpClient().SendMailAsync(mailMessage);
+        }
+    }
+
+    public class EmailHelpers
+    {
+        public static SmtpClient GetSmtpClient()
+        {
+            SmtpClient smtpClient = new SmtpClient(EmailServiceCredentials.EmailSMTPUrl);
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential(EmailServiceCredentials.EmailFromAddress, EmailServiceCredentials.EmailSMTPPasswordHash);
+
+            return smtpClient;
+        }
+
+        public static MailMessage GenerateMailMessage(string destination, string subject, string body)
+        {
+            MailMessage mailMessage = new MailMessage(new MailAddress(EmailServiceCredentials.EmailFromAddress, EmailServiceCredentials.EmailFromName), new MailAddress(destination));
+            mailMessage.Subject = EmailServiceCredentials.EmailAppName + " - " + subject;
+            mailMessage.Body = body;
+            mailMessage.IsBodyHtml = true;
+
+            return mailMessage;
         }
     }
 
--- WebAppTemplate/Controllers/AccountController.cs
+++ WebAppTemplate/Controllers/AccountController.cs
@@ -114,10 +114,11 @@ namespace WebAppTemplate.Controllers
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
+                    TempData["SuccessMessage"] = "Registration successful! Please check your email to confirm your account";
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
@@ -158,7 +159,7 @@ namespace WebAppTemplate.Controllers
             if (ModelState.IsValid)
             {
                 var user = await UserManager.FindByNameAsync(model.Email);
-                if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
+                if (user == null)
                 {
                     // Don't reveal that the user does not exist or is not confirmed
                     return View("ForgotPasswordConfirmation");
@@ -166,10 +167,10 @@ namespace WebAppTemplate.Controllers
 
                 // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
-                // string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
-                // await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                // return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
+                var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
+                await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
 
             // If we got this far, something failed, redisplay form
