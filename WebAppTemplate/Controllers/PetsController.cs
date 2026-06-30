c17465b430de31ab6177027fb4f272c96af473ee
faustjoe <joeyfausto107@gmail.com>
2026-06-30 07:44:26 -0700

    Delete legacy Add/Remove ActionResults from Booking and Pets that were used for testing and already previously replaced

--- WebAppTemplate/Controllers/PetsController.cs
+++ WebAppTemplate/Controllers/PetsController.cs
@@ -39,42 +39,6 @@ namespace PawesomePalace.Controllers
             return View(viewModel);
         }
 
-        // GET: /Pets/Add?petName=Steve&breed=GermanShepherd&age=3&ownerId=...
-        public ActionResult Add(string petName, string breed, Guid? ownerId = null)
-        {
-            ApplicationDbContext dbContext = new ApplicationDbContext();
-
-            var pet = new PetModel();
-
-            pet.OwnerId = ownerId;
-            pet.Name = petName;
-            pet.Breed = breed;
-
-            dbContext.PetModels.Add(pet);
-            dbContext.SaveChanges();
-
-            return Content($"Pet '{pet.Name}' added with ID: {pet.PetId}");
-
-        }
-
-        // GET: /Pets/Remove?petId=...
-        public ActionResult Remove(Guid petId)
-        {
-            ApplicationDbContext dbContext = new ApplicationDbContext();
-
-            var pet = dbContext.PetModels.Find(petId);
-
-            if (pet == null)
-            {
-                return Content("Error: Pet not found.");
-            }
-
-            dbContext.PetModels.Remove(pet);
-            dbContext.SaveChanges();
-
-            return Content($"Pet '{pet.Name}' removed.");
-        }
-
         // GET: /Pets/Create
         [HttpGet]
         public ActionResult Create()
